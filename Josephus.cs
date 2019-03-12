246b168bcb1ccc30d1012a39a882878aa0045f06
sharada <sharada98s@gmail.com>
2019-03-12 07:43:36 +0530

    committed

---
+++ Josephus.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Josephus
+{
+    class Program
+    {
+        static int josephus(int n, int k)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return (josephus(n - 1, k)
+                           + k - 1) % n + 1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of people");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int k = 2;
+            Console.WriteLine("The winning "
+            + "position is " + josephus(n, k));
+        }
+    }
+}
