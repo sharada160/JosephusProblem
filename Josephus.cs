6895e12bde051ae47d08da321483884b1ceeddce
sharada <sharada98s@gmail.com>
2019-03-16 11:42:07 +0530

    changes made

--- Josephus.cs
+++ Josephus.cs
@@ -2,11 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Josephus
 {
-    class Program
+    class CalWinningPosition
     {
         static int josephus(int n, int k)
         {
